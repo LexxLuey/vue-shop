bae889584135460139c2108c8a343b13280b357b
LexxLuey <biggestluey@gmail.com>
2021-10-12 14:41:28 +0100

    first commit

---
+++ src/views/Products.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <main class="wrapper">
+      <h1>Products</h1>
+
+      <div class="card-container">
+        <ProductCard
+          v-for="(product, index) in inventory"
+          :key="product.id"
+          :index="index"
+          :product="product"
+          :addToCart="addToCart"
+          class="card"
+        />
+
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Products',
+  props: ['inventory', 'addToCart'],
+
+  methods: {
+
+  },
+  components: {
+    ProductCard
+  }
+}
+</script>
